c723fd9dbe11fe678d5b5ab94fc2405e1b079b2b
dswbx <dennis.senn@gmx.ch>
2024-12-04 15:40:36 +0100

    revert to astro ^4 because of astro-db issues + added example how to access astro-db locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [react()],
-   /* vite: {
-      optimizeDeps: {
-         include: ["lodash-es"],
-      },
+   output: "hybrid",
+   integrations: [db(), react()],
+   vite: {
       ssr: {
-         external: ["react", "react-dom"],
-         noExternal: ["bknd", "lodash-es"],
+         external: ["@libsql/client"],
       },
-   }, */
+   },
 });
